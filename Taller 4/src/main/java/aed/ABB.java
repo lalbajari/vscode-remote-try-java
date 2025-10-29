eea7106999d15186515e392ccdd1d898625aa14c
lalbajari <lisa.albajari@gmail.com>
2025-10-29 22:25:55 +0000

    java taller

---
+++ Taller 4/src/main/java/aed/ABB.java
@@ -0,0 +1,64 @@
+package aed;
+
+import java.util.*;
+
+// Todos los tipos de datos "Comparables" tienen el método compareTo()
+// elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
+public class ABB<T extends Comparable<T>> {
+    // Agregar atributos privados del Conjunto
+
+    private class Nodo {
+        // Agregar atributos privados del Nodo
+
+        // Crear Constructor del nodo
+    }
+
+    public ABB() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public int cardinal() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public T minimo(){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public T maximo(){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void insertar(T elem){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public boolean pertenece(T elem){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void eliminar(T elem){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public String toString(){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public class ABB_Iterador {
+        private Nodo _actual;
+
+        public boolean haySiguiente() {            
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    
+        public T siguiente() {
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    }
+
+    public ABB_Iterador iterador() {
+        return new ABB_Iterador();
+    }
+
+}
